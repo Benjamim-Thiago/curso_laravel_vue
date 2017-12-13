31a874357088079d8b498383f58f269633972e14
Benjamim Thiago <bebenjamimthiago@gmail.com>
2017-12-13 08:51:48 -0300

    first

---
+++ resources/assets/js/components/PanelComponent.vue
@@ -0,0 +1,19 @@
+<template>
+
+    <div class="panel panel-default">
+        <div class="panel-heading">Example Component</div>
+
+        <div class="panel-body">
+            <h1>Teste</h1>
+            <h1>Teste</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
